a08a2ea799e4a89d9e1d6a6fa660e7b1c66b89ee
topbosco <967409+topbosco@users.noreply.github.com>
2022-02-22 17:52:01 +0100

    Add PurchasePersister

--- src/Controller/Purchase/PurchaseConfirmationController.php
+++ src/Controller/Purchase/PurchaseConfirmationController.php
@@ -4,9 +4,8 @@ namespace App\Controller\Purchase;
 
 use App\Cart\CartService;
 use App\Entity\Purchase;
-use App\Entity\PurchaseItem;
 use App\Form\CartConfirmationType;
-use DateTime;
+use App\Purchase\PurchasePersister;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -22,11 +21,13 @@ class PurchaseConfirmationController extends AbstractController
 {
     protected $cartService;
     protected $em;
+    protected $persister;
 
-    public function __construct(CartService $cartService, EntityManagerInterface $em)
+    public function __construct(CartService $cartService, EntityManagerInterface $em, PurchasePersister $persister)
     {
         $this->cartService = $cartService;
         $this->em = $em;
+        $this->persister = $persister;
     }
 
     #[Route('/purchase/confirm', name: 'purchase_confirm')]
@@ -52,8 +53,9 @@ class PurchaseConfirmationController extends AbstractController
         }
 
         // 3. Si je ne suis pas connecté : dégager (Security)
+        // FAIT AVEC LES ANNOTATIONS IsGranted
         // $user = $this->security->getUser();
-        $user = $this->getUser();
+        // $user = $this->getUser();
 
         // CECI EST FAIT AVEC UNE Annotation
         // if (!$user) {
@@ -74,33 +76,7 @@ class PurchaseConfirmationController extends AbstractController
         /** @var Purchase */
         $purchase = $form->getData();
 
-        // 6. Nous allons la lier avec l'utilisateur actuellement connecté (Security)
-        $purchase
-            ->setUser($user)
-            ->setPurchasedAt(new DateTime())
-            ->setTotal($this->cartService->getTotal())
-        ;
-
-        $this->em->persist($purchase);
-
-        // 7. Nous allons la lier avec les produits qui sont dans le panier (CartService)
-        foreach ($this->cartService->getDetailedCartItems() as $cartItem) {
-            $purchaseItem = new PurchaseItem();
-
-            $purchaseItem
-                ->setPurchase($purchase)
-                ->setProduct($cartItem->product)
-                ->setProductName($cartItem->product->getName())
-                ->setQuantity($cartItem->qty)
-                ->setTotal($this->cartService->getTotal())
-                ->setProductPrice($cartItem->product->getPrice())
-            ;
-
-            $this->em->persist($purchaseItem);
-        }
-
-        // 8. Nous allons enregistrer la commande (EntityManagerInterface)
-        $this->em->flush();
+        $this->persister->storePurchase($purchase);
 
         $this->cartService->empty();
 
