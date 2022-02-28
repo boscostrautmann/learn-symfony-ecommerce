2b97d6f39ba0ba758702dc12fee49cb6d5ce99d9
topbosco <967409+topbosco@users.noreply.github.com>
2022-02-28 15:29:38 +0100

    Add Event for Email PurchasePaymentSuccess

--- src/Controller/Purchase/PurchasePaymentSuccessController.php
+++ src/Controller/Purchase/PurchasePaymentSuccessController.php
@@ -4,17 +4,19 @@ namespace App\Controller\Purchase;
 
 use App\Cart\CartService;
 use App\Entity\Purchase;
+use App\Event\PurchaseSuccessEvent;
 use App\Repository\PurchaseRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PurchasePaymentSuccessController extends AbstractController
 {
     #[Route('/purchase/terminate/{id}', name: 'purchase_payment_success')]
     #[IsGranted('ROLE_USER')]
-    public function success($id, PurchaseRepository $purchaseRepository, EntityManagerInterface $em, CartService $cartService)
+    public function success($id, PurchaseRepository $purchaseRepository, EntityManagerInterface $em, CartService $cartService, EventDispatcherInterface $dispatcher)
     {
         // 1. Je récupère la commande
         $purchase = $purchaseRepository->find($id);
@@ -35,6 +37,10 @@ class PurchasePaymentSuccessController extends AbstractController
 
         // 3. Je vide le panfa-inverse
         $cartService->empty();
+
+        // 3.1 Lancer un événement qui permet aux autre dévéloppeurs de réagir à la prise d'une commande
+        $purchaseEvent = new PurchaseSuccessEvent($purchase);
+        $dispatcher->dispatch($purchaseEvent, 'purchase.success');
 
         // 4. Je redirige avec un flash vers la liste des commandes
         $this->addFlash('success', 'La commande a été payé et confirmée !');
