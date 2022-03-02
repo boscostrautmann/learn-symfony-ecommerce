92f6bfcef862176b79ce1e3e98214a19d680bf58
topbosco <967409+topbosco@users.noreply.github.com>
2022-03-02 15:57:47 +0100

    setup sessions in database

---
+++ migrations/Version20220302143425.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220302143425 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE sessions (sess_id VARBINARY(255) NOT NULL PRIMARY KEY, sess_data BLOB NOT NULL, sess_lifetime INT UNSIGNED NOT NULL, sess_time INT UNSIGNED NOT NULL, INDEX `sessions_sess_lifetime_idx` (`sess_lifetime`)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE session');
+    }
+}
---
+++ src/Entity/Sessions.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SessionsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SessionsRepository::class)]
+class Sessions
+{
+    #[ORM\Id]
+    #[ORM\Column(type: 'binary')]
+    private $sess_id;
+
+    #[ORM\Column(type: 'blob')]
+    private $sess_data;
+
+    #[ORM\Column(type: 'integer')]
+    private $sess_lifetime;
+
+    #[ORM\Column(type: 'integer')]
+    private $sess_time;
+
+    public function getSessId()
+    {
+        return $this->sess_id;
+    }
+
+    public function setSessId($sess_id): self
+    {
+        $this->sess_id = $sess_id;
+
+        return $this;
+    }
+
+    public function getSessData()
+    {
+        return $this->sess_data;
+    }
+
+    public function setSessData($sess_data): self
+    {
+        $this->sess_data = $sess_data;
+
+        return $this;
+    }
+
+    public function getSessLifetime(): ?int
+    {
+        return $this->sess_lifetime;
+    }
+
+    public function setSessLifetime(int $sess_lifetime): self
+    {
+        $this->sess_lifetime = $sess_lifetime;
+
+        return $this;
+    }
+
+    public function getSessTime(): ?int
+    {
+        return $this->sess_time;
+    }
+
+    public function setSessTime(int $sess_time): self
+    {
+        $this->sess_time = $sess_time;
+
+        return $this;
+    }
+}
